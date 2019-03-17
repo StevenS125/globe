c511de00c39b4f9f0369230ab9b12bf817d0a053
StevenS125 <sts12589@gmail.com>
2019-03-17 11:31:26 -0400

    added new routing functionality for navigation

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -1,10 +1,14 @@
+<?php 
+if (!isset($page_title)) { $page_title = 'Staff Area';}
+?>
+
 <!doctype html>
 
 <html lang="en">
   <head>
-    <title>GBI</title>
+    <title>GBI <?php echo $page_title; ?></title>
     <meta charset="utf-8">
-    <link rel="stylesheet" media="all" href="../stylesheets/staff.css">
+    <link rel="stylesheet" media="all" href="<?php echo url_for('./stylesheets/staff.css')?>">
   </head>
 
   <body>
@@ -15,6 +19,6 @@
 
     <nav>
         <ul>
-            <li><a href="index.php">Menu</a></li>
+            <li><a href="<?php echo url_for('./staff/index.php'); ?>">Menu</a></li>
         </ul>
     </nav>
