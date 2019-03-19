8d9e298ead02618453bfe98faf05a4478a3529ad
StevenS125 <sts12589@gmail.com>
2019-03-18 21:22:36 -0400

    added sql functionailty and mysqli api calls for table data

--- private/initialize.php
+++ private/initialize.php
@@ -22,5 +22,9 @@ $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
 define("WWW_ROOT", $doc_root);
 
   require_once('functions.php');
+  require_once('database.php');
+  require_once('queries.php');
+
+  $db = db_connect();
 
 ?>
