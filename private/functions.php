78985939d24d2e179aac1d88f78d693f18618ec3
StevenS125 <sts12589@gmail.com>
2019-03-21 10:47:28 -0400

    validation of form added to subjects

--- private/functions.php
+++ private/functions.php
@@ -43,5 +43,20 @@ function url_for($script_path) {
     return $_SERVER['REQUEST_METHOD'] == 'GET';
   }
   
+  function display_errors($errors=array()) {
+    $output = '';
+    if(!empty($errors)) {
+      $output .= "<div class=\"errors\">";
+      $output .= "Please fix the following errors:";
+      $output .= "<ul>";
+      foreach($errors as $error) {
+        $output .= "<li>" . h($error) . "</li>";
+      }
+      $output .= "</ul>";
+      $output .= "</div>";
+    }
+    return $output;
+  }
+  
 
 ?>
